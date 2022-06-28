6d7736c669664cfe36f103abd007b8ce422f0d20
afaqm <afaqm@AWG_DEVELOPER>
2022-06-28 23:15:54 +0500

    Add project files.

---
+++ myapp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+    public class GalleryController : Controller
+    {
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+    }
+    public class loginController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+    }
+    public class entryController : Controller
+    {
+        public IActionResult entry()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ myapp/wwwroot/js/JavaScript.js
@@ -0,0 +1,11 @@
+﻿function log() {
+
+    var Username = document.getElementById("Username").value;
+    var Password = document.getElementById("Password").value;
+    if (Username == "Afaq" && Password == "Afaq3179") {
+
+        window.open("http://127.0.0.1:5501/entry.html");
+    } else {
+        alert("Username or Passcode is incorrect");
+    }
+}
